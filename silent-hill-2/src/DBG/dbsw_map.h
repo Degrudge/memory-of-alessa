037256b108e4b391608a4f9e1196127efdee930b
Degrudge <313046625+Degrudge@users.noreply.github.com>
2026-09-15 16:22:38 +0000

    sh2: Match `DBG/dbsw_map` (#347)

--- silent-hill-2/src/DBG/dbsw_map.h
+++ silent-hill-2/src/DBG/dbsw_map.h
@@ -1,6 +1,15 @@
 #ifndef DBSW_MAP_H
 #define DBSW_MAP_H
 
-char * dbSwitchMapHelp(signed int Y /* r2 */);
+#include "common.h"
+
+#include "DBG/dbsw_sys.h"
+#include "DBG/dbswitch.h"
+
+char* dbSwitchMapHelp(int Y /* r2 */);
+
+void dbSwitchMapPrint(void);
+
+void dbSwitchMapInit(void);
 
 #endif // DBSW_MAP_H
