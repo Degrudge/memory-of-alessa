94a60896915b46faf20252e4a2337b211726d714
Degrudge <313046625+Degrudge@users.noreply.github.com>
2026-09-15 06:59:56 +0000

    sh2: Match `DBG/dbsw_all` (#342)
    * match `DBG/dbsw_all`
    
    compiler's complaining over implicit conversions and missing symbols related to `DBG/dbsw_sys`
    
    * add missing declarations in header files
    
    * clean up
    
    * superfluous parenthesis

--- silent-hill-2/src/DBG/dbsw_all.h
+++ silent-hill-2/src/DBG/dbsw_all.h
@@ -1,4 +1,17 @@
 #ifndef DBSW_ALL_H
 #define DBSW_ALL_H
 
+#include "SH2_common/pad.h"
+#include "DBG/dbsw_sys.h"
+#include "DBG/dbswitch.h"
+#include "DBG/dbsw_map.h"
+
+void dbSwitchAllInit(int enable /* r16 */);
+
+static void dbSwitchHelpPrint(void);
+
+int dbSwitchIsVisible(void);
+
+void dbSwitchAllPrint(void);
+
 #endif // DBSW_ALL_H
