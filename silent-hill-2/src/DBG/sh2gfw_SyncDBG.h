0ee6d47f5e587a61fd0a331539fa59e198cf9d8a
Degrudge <313046625+Degrudge@users.noreply.github.com>
2026-09-17 18:57:52 +0300

    Update sh2gfw_SyncDBG.h

--- silent-hill-2/src/DBG/sh2gfw_SyncDBG.h
+++ silent-hill-2/src/DBG/sh2gfw_SyncDBG.h
@@ -1,4 +1,16 @@
 #ifndef SH2GFW_SYNCDBG_H
 #define SH2GFW_SYNCDBG_H
 
+void sh2gfw_Init_AllVertCounter(void);
+
+void sh2gfw_Incliment_VertNumIndices(void);
+
+void sh2gfw_Add_SemiTransVertNum(int num /* r2 */);
+
+void sh2gfw_Add_VertexNum(int num /* r2 */);
+
+void sh2gfw_init_Perf(void);
+
+void sh2gfw_Store_Perf2(int count /* r2 */, int md /* r2 */);
+
 #endif // SH2GFW_SYNCDBG_H
