f3b925aa14e4feb7fdceb71ff93d32c1407bd44e
Degrudge <313046625+Degrudge@users.noreply.github.com>
2026-09-18 07:58:44 +0300

    Update dbflag.h

--- silent-hill-2/src/DBG/dbflag.h
+++ silent-hill-2/src/DBG/dbflag.h
@@ -3,7 +3,10 @@
 
 #include "common.h"
 
-u_int dbFlag(u_int flag /* r2 */);
 int dbFlagReserve(u_int flag /* r2 */, char* explain_message /* r2 */);
+
+u_int dbFlag(u_int flag /* r2 */);
+
+void dbFlagSet(u_int flag /* r2 */);
 
 #endif // DBFLAG_H
