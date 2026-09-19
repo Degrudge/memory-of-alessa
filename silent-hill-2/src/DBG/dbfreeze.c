e16bea05aa3faee3599f75380fa99efe320ec2b3
Degrudge <313046625+Degrudge@users.noreply.github.com>
2026-09-19 17:24:07 +0000

    sh2: Match `DBG/dbfreeze` (#354)

---
+++ silent-hill-2/src/DBG/dbfreeze.c
@@ -0,0 +1,46 @@
+#include "dbfreeze.h"
+
+
+
+
+
+void dbFreeze(void) {
+    int loop; // r5
+    u_char paddata[32]; // r29+0x10
+    static int fz_step; // @ 0x011B6F48
+    if (shPadGetPort() == 1) {
+        do {
+
+            dbFlowSetCheckPoint("dbfreeze loop");
+            libShPadRead(1, 0, &paddata);
+            shSysKeyNormalize(&paddata);
+
+            loop = true;
+            switch (fz_step) {
+                case 0:
+                    loop = false;
+                    if (paddata[22])      fz_step = 1;
+                    break;
+                case 1:
+                    if (!paddata[22])     fz_step = 2;
+                    break;
+                case 2:
+                    if (paddata[22])      fz_step = 3;
+                    else if (paddata[17]) fz_step = 4;
+                    else if (paddata[19]) loop = false;
+                    break;
+                case 3:
+                    if (!paddata[22]) fz_step = 0;
+                    break;
+                case 4:
+                    loop = false;
+                    fz_step = 5;
+                    break;
+                case 5:
+                    if (!paddata[17]) fz_step = 2;
+                    break;
+            }
+        } while (loop);
+        dbFlowSetCheckPoint("exit dbfreeze");
+    }
+}
