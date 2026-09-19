992272ba1e6cdccc777ba1c47d967a8cfbbe5dcc
Degrudge <313046625+Degrudge@users.noreply.github.com>
2026-09-19 09:49:01 +0000

    sh2: Match `DBG/dbflow` (#352)
    * Update SLUS_202.28.yaml
    
    * Update `DBG/dbflow.h`
    
    * Create `DBG/dbflow.c`
    
    * Apply suggestion from @dreamingmoths
    
    Co-authored-by: dreamingmoths <mothdreams666@gmail.com>
    
    * replace printf with DEBUG macro
    
    ---------
    
    Co-authored-by: dreamingmoths <mothdreams666@gmail.com>

--- silent-hill-2/src/DBG/dbflow.h
+++ silent-hill-2/src/DBG/dbflow.h
@@ -1,9 +1,20 @@
 #ifndef DBFLOW_H
 #define DBFLOW_H
+
 #include "sh2_common.h"
 #include "debug.h"
 
+#include "Multi_thr/intc/syncv.h"
+
 #define dbFlowSetCheckPoint(checkpoint) ___dbFlowSetCheckPoint("`" checkpoint "'(" __FILE__ ":" ASSTR(__LINE__) ")")
 #define dbFlowSetCheckPointOnLine(checkpoint, line) ___dbFlowSetCheckPoint("`" checkpoint "'(" __FILE__ ":" ASSTR(line) ")")
+
+void ___dbFlowSetCheckPoint(char * check_point /* r2 */);
+
+int dbFlowCheck(void);
+
+void dbFlowStartCheck(int enable /* r2 */);
+
+void dbFlowCheckSleepTime(int flow_ok /* r2 */);
 
 #endif
