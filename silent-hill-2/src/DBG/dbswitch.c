3e15e7c0b4d2dc5deb88a39b47f92e152ff1f4ae
degrudge <313046625+Degrudge@users.noreply.github.com>
2026-09-19 09:09:43 +0300

    corrections, data split, and coin flip

--- silent-hill-2/src/DBG/dbswitch.c
+++ silent-hill-2/src/DBG/dbswitch.c
@@ -5,10 +5,11 @@ extern /* static */ int dbSwitchX; // size: 0x4, address: 0x11B6E48
 extern /* static */ int dbSwitchY; // size: 0x4, address: 0x11B6E50
 extern /* static */ int dbSwitchDispF; // size: 0x4, address: 0x11B6E58
 
-void dbSwitchDispIndicator(int enable /* r2 */, int o /* r2 */, int x /* r2 */, int dx /* r2 */, int dy /* r2 */) {
+#line 17
+void dbSwitchDispIndicator(signed int enable /* r2 */, signed int o /* r2 */, signed int x /* r2 */, signed int dx /* r2 */, signed int dy /* r2 */) {
     int just, bit, Y; // r2
-    char* sym[2][2] = {"-\n", "\x8a-\n", "0\n", "\x8a\x4f\n"}; // r29+0x20
-
+    char* sym[2][2] = {"-\n", "\x8a-\n", "O\n", "\x8aO\n"}; // r29+0x20
+    int *temp;
     if (enable) {
 
         dy = -dy;
@@ -22,24 +23,27 @@ void dbSwitchDispIndicator(int enable /* r2 */, int o /* r2 */, int x /* r2 */, 
 
             dbSwitchX += dx;
             dbSwitchY += dy;
-            if (dbSwitchX < 0)     dbSwitchX = 0;
-            if (dbSwitchX > 1)     dbSwitchX = 1;
-            if (dbSwitchY < 0)     dbSwitchY = 0;
-            if (dbSwitchY > 31u)   dbSwitchY = 31;
+            if (dbSwitchX < 0)                   dbSwitchX = 0;
+            if (dbSwitchX > 1)                   dbSwitchX = 1;
+            if (dbSwitchY < 0)                   dbSwitchY = 0;
+            if (dbSwitchY > sizeof(int) * 8 - 1) dbSwitchY = 31;
             if (o) {
-                dbSwitchStat[dbSwitchX] = dbSwitchStat[dbSwitchX] ^ 1 << dbSwitchY;
+                temp = &dbSwitchStat[dbSwitchX];
+                FLIP_BIT(*temp, dbSwitchY);
             }
             dbfntprintfR("%1X\n", dbSwitchX);
+            for (just = 0; just < sizeof(int) * 8; just++) {
 
-            for (just = 0; just < 32u; just++) {
 
-                if (!(just % 8)) dbfntprintR("\n");
-
+                if (!(just % 8)) {
+                    dbfntprintR("\n");
+                }
                 bit = (dbSwitchStat[dbSwitchX] & 1 << just) != 0;
                 Y = dbSwitchY == just;
                 dbfntprintR(sym[bit][Y]);
             }
         }
+
     }
 }
 
@@ -47,13 +51,15 @@ void dbSwitchDispIndicator(int enable /* r2 */, int o /* r2 */, int x /* r2 */, 
 
 
 int dbSwitchDispEnable(int enable) {
-    if (enable < 0) enable = dbSwitchDispF;
-    else dbSwitchDispF = enable;
-    return enable;
-}
+    if (enable < 0) enable = dbSwitchDispF; else dbSwitchDispF = enable; return enable;
 
+
+
+}
 int dbSwitchInit(DBSW_ID _X, u_long bit_pattern) {
     int X;
+
+
     X = _X;
 
     if (X < DBSW_SYS) return 0;
@@ -66,12 +72,13 @@ int dbSwitchInit(DBSW_ID _X, u_long bit_pattern) {
 int dbSwitchSet(DBSW_ID _X /* r2 */, int Y /* r2 */, int set /* r2 */) {
     int X;
     int *temp;
+
     X = _X;
 
-    if (X < DBSW_SYS)   return 0;
-    if (X > DBSW_MAP)   return 0;
-    if (Y < 0)          return 0;
-    if (Y > 31u)        return 0;
+    if (X < DBSW_SYS)            return 0;
+    if (X > DBSW_MAP)            return 0;
+    if (Y < 0)                   return 0;
+    if (Y > sizeof(int) * 8 - 1) return 0;
 
     if (set) {
         temp = &dbSwitchStat[X];
@@ -84,17 +91,16 @@ int dbSwitchSet(DBSW_ID _X /* r2 */, int Y /* r2 */, int set /* r2 */) {
 
 int dbSwitch(DBSW_ID _X /* r2 */, int Y /* r2 */) {
     int X;
+
     X = _X;
 
-    if (X < DBSW_SYS)   return 0;
-    if (X > DBSW_MAP)   return 0;
-    if (Y < 0)          return 0;
-    if (Y > 31u)        return 0;
-
+    if (X < DBSW_SYS)            return 0;
+    if (X > DBSW_MAP)            return 0;
+    if (Y < 0)                   return 0;
+    if (Y > sizeof(int) * 8 - 1) return 0;
     if (dbSwitchDispF) {
         return (dbSwitchStat[X] & 1 << Y) != 0;
     }
-
     return 0;
 }
 
@@ -103,6 +109,5 @@ int dbSwitch(DBSW_ID _X /* r2 */, int Y /* r2 */) {
 int dbSwitchGetPos(int * Xp /* r2 */, int * Yp /* r2 */) {
     if (Xp) *Xp = dbSwitchX;
     if (Yp) *Yp = dbSwitchY;
-
     return dbSwitch(dbSwitchX, dbSwitchY);
 }
