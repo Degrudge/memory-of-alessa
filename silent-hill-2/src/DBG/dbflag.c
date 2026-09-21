92369ea23ba996cb1b7979d49b553d2dd88cca44
Degrudge <313046625+Degrudge@users.noreply.github.com>
2026-09-21 06:56:51 +0300

    corrections

--- silent-hill-2/src/DBG/dbflag.c
+++ silent-hill-2/src/DBG/dbflag.c
@@ -2,47 +2,45 @@
 
 extern /* static */ u_int dbflag_reserved; // size: 0x4, address: 0x116DD40
 extern /* static */ char* dbflag_explain[32]; // size: 0x80, address: 0x33DE50
-extern int execEnv_debug_flag; // size: 0x4, address: 0x5526A0
+// extern int execEnv_debug_flag; // size: 0x4, address: 0x5526A0
 
+#line 38
 int dbFlagReserve(u_int flag /* r2 */, char * explain_message /* r2 */) {
     u_int check; // r16
     char** explain; // r17
     if (dbflag_reserved & flag) {
 
-        verbose(1,
-                "dbflag.c:44> can't reserved debug flag : %08x : %s\n",
-                flag,
-                explain_message);
 
-        verbose(2,
-                "dbflag.c:46> alreday reserved          : %08x\n",
-                dbflag_reserved);
+        VERBOSE(1, "can't reserved debug flag : %08x : %s\n", flag, explain_message);
 
-        for (check = 1, explain = dbflag_explain; check != 0; check*=2, explain++) {
+        VERBOSE(2, "alreday reserved          : %08x\n", dbflag_reserved);
+
+
+
+        for (check = 1, explain = dbflag_explain; check != 0; check<<=1, explain++) {
             if (check & dbflag_reserved) {
-                verbose(3,
-                        "dbflag.c:52> flag report               : %08x : %s\n", check,
-                        *explain ? *explain : "(no message: why?)");
+                VERBOSE(3, "flag report               : %08x : %s\n", check, *explain ? *explain : "(no message: why?)");
+
             }
         }
         return 0;
     }
-    for (check = 1, explain = dbflag_explain; check != 0; check*=2, explain+=1) {
+    for (check = 1, explain = dbflag_explain; check != 0; check<<=1, explain++) {
         if (check & flag) {
             dbflag_reserved |= check;
             *explain = explain_message;
         }
     }
-    verbose(4,
-            "dbflag.c:64> reserved debug flag : %08x : %s\n",
-            flag,
-            explain_message);
+    VERBOSE(4, "reserved debug flag : %08x : %s\n", flag, explain_message);
     return 1;
 }
+
+
 
 u_int dbFlag(u_int flag) {
     return execEnv_debug_flag & flag;
 }
+
 
 void dbFlagSet(u_int flag) {
     execEnv_debug_flag |= flag;
--- silent-hill-2/src/DBG/dbflag.h
+++ silent-hill-2/src/DBG/dbflag.h
@@ -2,6 +2,9 @@
 #define DBFLAG_H
 
 #include "common.h"
+#include "debug.h"
+
+#include "Exec_Env/exec_env.h"
 
 int dbFlagReserve(u_int flag /* r2 */, char* explain_message /* r2 */);
 
