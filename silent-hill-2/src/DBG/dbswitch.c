5e91e45c2f96584d00b508189e1a1d8f51177588
moth dreams <mothdreams666@gmail.com>
2026-09-20 17:53:04 -0700

    sh2: match `wrap_printf`, move dbswitch -> c

---
+++ silent-hill-2/src/DBG/dbswitch.c
@@ -0,0 +1,25 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/DBG/dbswitch", dbSwitchDispIndicator);
+
+INCLUDE_ASM("asm/nonmatchings/DBG/dbswitch", dbSwitchDispEnable);
+
+INCLUDE_ASM("asm/nonmatchings/DBG/dbswitch", dbSwitchInit);
+
+INCLUDE_ASM("asm/nonmatchings/DBG/dbswitch", dbSwitchSet);
+
+INCLUDE_ASM("asm/nonmatchings/DBG/dbswitch", dbSwitch);
+
+INCLUDE_ASM("asm/nonmatchings/DBG/dbswitch", dbSwitchGetPos);
+
+INCLUDE_RODATA("asm/nonmatchings/DBG/dbswitch", @4);
+
+INCLUDE_RODATA("asm/nonmatchings/DBG/dbswitch", @5_0x00397068);
+
+INCLUDE_RODATA("asm/nonmatchings/DBG/dbswitch", @6);
+
+INCLUDE_RODATA("asm/nonmatchings/DBG/dbswitch", @7_0x00397078);
+
+INCLUDE_RODATA("asm/nonmatchings/DBG/dbswitch", @38_0x00397080);
+
+INCLUDE_RODATA("asm/nonmatchings/DBG/dbswitch", @39_0x00397088);
