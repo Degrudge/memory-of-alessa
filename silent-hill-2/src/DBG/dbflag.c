17d13f552bdcb93939d0c796d9a8135095d5ef75
Degrudge <313046625+Degrudge@users.noreply.github.com>
2026-09-21 09:31:56 +0300

    add todo

--- silent-hill-2/src/DBG/dbflag.c
+++ silent-hill-2/src/DBG/dbflag.c
@@ -2,7 +2,7 @@
 
 extern /* static */ u_int dbflag_reserved; // size: 0x4, address: 0x116DD40
 extern /* static */ char* dbflag_explain[32]; // size: 0x80, address: 0x33DE50
-static const char no_msg[] = "(no message: why?)";
+static const char no_msg[] = "(no message: why?)"; //@todo: string from dead code?
 
 #line 38
 int dbFlagReserve(u_int flag /* r2 */, char * explain_message /* r2 */) {
